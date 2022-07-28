556c048f926b2600750bf9af223dee2cdf555a59
AGUS DHARMAWAN <96321168+agdhr@users.noreply.github.com>
2022-07-28 15:33:51 +0700

    Add files via upload

--- sgmColorMasking.py
+++ sgmColorMasking.py
@@ -2,20 +2,17 @@
 # https://machinelearningknowledge.ai/image-segmentation-in-python-opencv/#4_Segmentation_using_Color_Masking
 
 import cv2
-path = 'beach.png'
+import numpy as np
+
+path = 'orange.jpg'
 img = cv2.imread(path)
 
-rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-hvs = cv2.cvtColor(rgb, cv2.COLOR_RGB2HSV)
+hvs = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-light_blue = (90, 70, 50)
-dark_blue = (128, 255, 255)
+lower = np.array([13, 13, 80])
+upper = np.array([179, 255, 255])
 
-# You can use the following values for green
-# light_green = (40, 40, 40)
-# dark_greek = (70, 255, 255)
-
-mask = cv2.inRange(hvs, light_blue,dark_blue)
+mask = cv2.inRange(hvs, lower,upper)
 result = cv2.bitwise_and(img, img, mask = mask)
-cv2.imshow('Hasil', result)
+cv2.imshow('Result', result)
 cv2.waitKey(0)
